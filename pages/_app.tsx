98a74f45c27f6f1f2f2495fa2cfba06a01b28b9d
derek2403 <derekliew0@gmail.com>
2026-06-21 17:38:33 +0800

    feat(app): dapp-kit providers (SuiClient, Wallet, react-query)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
+import "@mysten/dapp-kit/dist/index.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SuiClientProvider, WalletProvider, createNetworkConfig } from "@mysten/dapp-kit";
+import { getJsonRpcFullnodeUrl } from "@mysten/sui/jsonRpc";
+
+const { networkConfig } = createNetworkConfig({
+  testnet: { url: getJsonRpcFullnodeUrl("testnet"), network: "testnet" },
+});
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider autoConnect>
+          <Component {...pageProps} />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
 }
